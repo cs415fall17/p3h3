8d5ffd5941ba690ee0779a9b02313a4ea174db36
David Tauraso <david318@me.com>
2017-10-25 22:10:55 -0700

    updated h3.py

--- h3.py
+++ h3.py
@@ -7,23 +7,38 @@ def hgcd(a, b):
 		return a
 	elif bothAreEven(a, b):
 
-		x = 2 * hgcd( a // 2, b // 2 )
-		return x
+		return 2 * hgcd( a // 2, b // 2 )
 
 	elif (not isEven(a)) and isEven(b):
-		x = hgcd( a, b // 2 )
-		return x
-	elif bothAreOdd(a, b) and (a - b) // 2 == a:
-		x = hgcd(b, a % b)
-		return x
+		return hgcd( a, b // 2 )
+
+	# extra part to prevent infinite call stack when a and b are odd and (a - b) // 2 == a
+	elif bothAreOdd(a, b) or (a - b) // 2 == a:
+		return hgcd(b, a % b)
 	elif bothAreOdd(a, b):
 		# |1 - 3| // 2 == 1
-		x = hgcd( (a - b) // 2, b )
-		return x
+		return hgcd( (a - b) // 2, b )
 	# extra part to prevent infinite call stack when a is even and b is odd
 	else:
-		x = hgcd(b, a % b)
-		return x
+		return hgcd(b, a % b)
+
+def hgcdv(a, b):
+	a = abs(a)
+	b = abs(b)
+	print('(', a, ',', b, ')')
+	# base case
+	if b == 0:
+		return a
+	elif bothAreEven(a, b):
+
+		return 2 * hgcd( a // 2, b // 2 )
+
+	elif (not isEven(a)) and isEven(b):
+		return hgcd( a, b // 2 )
+
+	elif bothAreOdd(a, b):
+		# |1 - 3| // 2 == 1
+		return hgcd( (a - b) // 2, b )
 
 def isEven(a):
 	if a % 2 == 0:
@@ -45,12 +60,37 @@ def bothAreOdd(a, b):
 		return False
 
 def gcd(a, b):
-	#print('(', a, ',', b, ')')
+	print('(', a, ',', b, ')')
 
 	if b == 0:
 		return a
 	return gcd(b, a % b)
 
+def hgcdCheck(a, b):
+	a = abs(a)
+	b = abs(b)
+	# base case
+	if b == 0:
+		return 1
+	elif bothAreEven(a, b):
+
+		return (2 * hgcdCheck( a // 2, b // 2 ))//2 + 1
+
+	elif (not isEven(a)) and isEven(b):
+		return hgcdCheck( a, b // 2 ) + 1
+	elif bothAreOdd(a, b) or (a - b) // 2 == a:
+		return hgcdCheck(b, a % b) + 1
+	elif bothAreOdd(a, b):
+		# |1 - 3| // 2 == 1
+		return hgcdCheck( (a - b) // 2, b ) + 1
+	# extra part to prevent infinite call stack when a is even and b is odd
+	else:
+		return hgcdCheck(b, a % b) + 1
+def gcdCheck(a, b):
+
+	if b == 0:
+		return 1
+	return gcdCheck(b, a % b) + 1
 #hgcd(25, 24)
 
 #print()
@@ -61,13 +101,263 @@ def gcd(a, b):
 #hgcd(21, 25)
 #print()
 #gcd(21, 25)
-print(hgcd(5, 3))
-print(gcd(5, 3))
-'''
+print("h is significantly slower")
+print(hgcd(98, 48))
+print(gcd(98, 48))
+print("hg is faster")
+print(hgcd(30, 52))
+print(gcd(30, 52))
+
+print("g is faster")
+print(hgcd(99, 70))
+print(gcd(99, 70))
+
+print(hgcdv(123, 87))
+exit()
 for i in range(100):
-	for j in range(i):
-		print(i, j)
-		print(hgcd(i, j))
-		print(gcd(i, j))
-		print()
+	for j in range(100):
+		if hgcdCheck(i, j) < gcdCheck(i, j):
+			print("hg is faster")
+			print(i, j)
+			print(hgcd(i, j), "\n",gcd(i, j))
+			#print("---------")
+			#print(hgcd(i, j))
+
+			#print()
+			#print(gcd(i, j))
+			#print("---------")
+			#print()
+		#elif hgcdCheck(i, j) > gcdCheck(i, j):
+		#	print("g is faster")
+		#	print(i, j)
+		#	print(hgcd(i, j), "\n",gcd(i, j))
+
+		elif hgcdCheck(i, j) >= 2 * gcdCheck(i, j):
+			print("h is significantly slower")
+			print(i, j)
+			print(hgcd(i, j), "\n",gcd(i, j))
+
+
+'''
+13 8
+19 12
+21 8
+21 13
+23 14
+29 8
+29 18
+29 21
+31 12
+31 18
+31 19
+31 20
+34 13
+34 21
+35 22
+37 8
+37 14
+37 23
+37 29
+39 24
+41 26
+43 12
+43 28
+43 31
+45 8
+45 26
+45 28
+45 37
+47 13
+47 18
+47 29
+47 30
+47 34
+49 18
+49 30
+49 31
+50 19
+50 21
+50 29
+50 31
+51 14
+51 20
+51 28
+51 31
+51 37
+51 40
+53 8
+53 34
+53 45
+55 12
+55 21
+55 34
+55 36
+55 43
+57 22
+57 35
+57 36
+57 44
+58 36
+59 38
+60 13
+60 23
+60 37
+60 47
+61 8
+61 38
+61 53
+63 24
+63 39
+65 14
+65 18
+65 40
+65 42
+65 47
+65 51
+66 29
+66 37
+67 12
+67 18
+67 26
+67 41
+67 44
+67 49
+67 55
+69 8
+69 19
+69 42
+69 50
+69 61
+71 20
+71 21
+71 26
+71 28
+71 43
+71 44
+71 45
+71 46
+71 50
+71 51
+71 56
+73 13
+73 28
+73 42
+73 45
+73 46
+73 60
+74 31
+74 43
+75 44
+75 46
+76 21
+76 29
+76 47
+76 55
+77 8
+77 30
+77 47
+77 50
+77 69
+79 12
+79 14
+79 22
+79 28
+79 29
+79 30
+79 48
+79 49
+79 50
+79 51
+79 52
+79 57
+79 65
+79 67
+80 31
+80 49
+81 31
+81 34
+81 47
+81 50
+81 52
+82 31
+82 37
+82 45
+82 51
+82 52
+83 18
+83 23
+83 54
+83 65
+85 8
+85 18
+85 54
+85 67
+85 77
+86 13
+86 73
+87 24
+87 34
+87 53
+87 54
+87 63
+87 68
+88 19
+88 37
+88 51
+88 69
+89 34
+89 55
+89 58
+91 12
+91 20
+91 36
+91 40
+91 51
+91 55
+91 56
+91 60
+91 71
+91 72
+91 79
+92 21
+92 35
+92 57
+92 71
+93 8
+93 14
+93 26
+93 36
+93 52
+93 54
+93 57
+93 60
+93 67
+93 79
+93 85
+94 36
+94 58
+95 29
+95 60
+95 62
+95 66
+97 21
+97 26
+97 37
+97 38
+97 59
+97 60
+97 62
+97 71
+97 76
+98 43
+98 45
+98 53
+98 55
+98 60
+99 13
+99 28
+99 38
+99 61
+99 71
+99 86
 '''
